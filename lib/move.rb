7a7cad87cc19101585a74555519d1c22339674b0
Kiley Bobbitt <jkileybob@aol.com>
2018-09-30 15:01:57 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(user_input)
 end
 
 def move(board, position, character = "X")
-  puts "the board is: #{board}"
+  puts "the board is #{board}"
   puts "the index is #{index}"
   puts "the character is #{character}"
   board[index] = character
